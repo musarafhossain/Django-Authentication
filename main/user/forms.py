0da137b09609719aba7c77a0b52fc756019b596e
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2025-01-10 02:46:49 +0530

    add design in registration page

--- main/user/forms.py
+++ main/user/forms.py
@@ -3,8 +3,44 @@ from django.contrib.auth.models import User
 from django import forms
 
 class RegistrationForm(UserCreationForm):
-    password2 = forms.CharField(label='Confirm password(again)', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Confirm password (again)',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Confirm your password'
+        })
+    )
+
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1']
         labels = {'email': 'Email'}
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter your username'
+            }),
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter your first name'
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter your last name'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter your email'
+            }),
+            'password1': forms.PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter your password'
+            }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Enter your password'
+        })

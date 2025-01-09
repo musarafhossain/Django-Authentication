fe8cdfe67ffaf158db9592f124b20efecb6b82e2
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2025-01-10 02:29:57 +0530

    complete registration function

--- main/user/views.py
+++ main/user/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from django.contrib import messages
 
 # Create your views here.
 def home_view(request):
@@ -8,7 +10,17 @@ def login_view(request):
     return render(request, 'login.html')
 
 def register_view(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        fm = RegistrationForm(request.POST)
+        if fm.is_valid():
+            fm.save()
+            messages.success(request, 'Registration successful. You can now login.')
+            return redirect('login')
+    else:
+        fm = RegistrationForm()
+    return render(request, 'register.html', {
+        'form' : fm,
+    })
 
 def profile_view(request):
     return render(request, 'profile.html')

6e7f44972752ac904c7f58aa8c800e9ccfc98416
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2025-01-11 02:55:47 +0530

    complete login functionalities

--- main/user/views.py
+++ main/user/views.py
@@ -1,15 +1,37 @@
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def home_view(request):
     return render(request, 'home.html')
 
 def login_view(request):
-    return render(request, 'login.html')
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == 'POST':
+        fm = AuthenticationForm(request, request.POST)
+        if fm.is_valid():
+            username = fm.cleaned_data['username']
+            password = fm.cleaned_data['username']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Logged in successfully !!')
+                return redirect('home')
+            else:
+                print("Login failed...")
+    else:
+        fm = AuthenticationForm()
+    return render(request, 'login.html', {
+        'form': fm
+    })
 
 def register_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
     if request.method == 'POST':
         fm = RegistrationForm(request.POST)
         if fm.is_valid():

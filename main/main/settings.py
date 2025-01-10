d79fd80047baa483f46ce1b8a946832c491176e1
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2025-01-11 02:06:57 +0530

    add css instead of bootstrap

--- main/main/settings.py
+++ main/main/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIR = f'{BASE_DIR}/static'
+STATICFILES_DIRS = [ STATIC_DIR ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- main/user/forms.py
+++ main/user/forms.py
@@ -6,7 +6,6 @@ class RegistrationForm(UserCreationForm):
     password2 = forms.CharField(
         label='Confirm password (again)',
         widget=forms.PasswordInput(attrs={
-            'class': 'form-control',
             'placeholder': 'Confirm your password'
         })
     )
@@ -17,23 +16,18 @@ class RegistrationForm(UserCreationForm):
         labels = {'email': 'Email'}
         widgets = {
             'username': forms.TextInput(attrs={
-                'class': 'form-control',
                 'placeholder': 'Enter your username'
             }),
             'first_name': forms.TextInput(attrs={
-                'class': 'form-control',
                 'placeholder': 'Enter your first name'
             }),
             'last_name': forms.TextInput(attrs={
-                'class': 'form-control',
                 'placeholder': 'Enter your last name'
             }),
             'email': forms.EmailInput(attrs={
-                'class': 'form-control',
                 'placeholder': 'Enter your email'
             }),
             'password1': forms.PasswordInput(attrs={
-                'class': 'form-control',
                 'placeholder': 'Enter your password'
             }),
         }
@@ -41,6 +35,5 @@ class RegistrationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['password1'].widget.attrs.update({
-            'class': 'form-control',
             'placeholder': 'Enter your password'
         })
